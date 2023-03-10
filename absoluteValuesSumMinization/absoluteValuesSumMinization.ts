0c29ccb47fbc28312196920793ad5f959b24de3d
Julio Delgado <juliods@gmail.com>
2023-03-10 16:47:45 +0000

    add

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,12 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+  const length = a.length;
+  const index = length % 2 === 0 ? length / 2 - 1 : Math.floor(length / 2);
+  return a[index];
 }
 
 // console.log(absoluteValuesSumMinimization([2, 4, 7]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+
+absoluteValuesSumMinimization([3, 2]);
