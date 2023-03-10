c35ff1514c03275c9d1bb84eaeec8ae967fbb493
Julio Delgado <juliods@gmail.com>
2023-03-10 17:08:00 +0000

    addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,8 @@
 export function addTwoDigits(n: any): number {
-
+  return n
+    .toString()
+    .split()
+    .reduce((acc, el) => (acc += Number(el)));
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
