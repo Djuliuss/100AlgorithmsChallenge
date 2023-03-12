45baa794701761a6b48369ca00eb5fb472e44746
Julio Delgado <juliods@gmail.com>
2023-03-12 15:31:01 +0000

    alternating sums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,13 @@
 export function alternatingSums(a: number[]): number[] {
-
+  let [totalWeightTeam1, totalWeightTeam2] = [0, 0];
+  a.forEach((e, index) => {
+    if (index % 2 === 0) {
+      totalWeightTeam1 += e;
+    } else {
+      totalWeightTeam2 += e;
+    }
+  });
+  return [totalWeightTeam1, totalWeightTeam2];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
