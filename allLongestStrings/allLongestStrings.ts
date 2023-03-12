a7682255847d993ff8488b7d1c2bf798400834aa
Julio Delgado <juliods@gmail.com>
2023-03-12 14:26:34 +0000

    alllongeststrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,12 @@
 export function allLongestStrings(inputArray: string[]): string[] {
-
+  const inputsByLength: { [length: string]: string[] } = {};
+  inputArray.forEach((input) => {
+    const length = input.length.toString();
+    inputsByLength[length] = inputsByLength[length] || [];
+    inputsByLength[length].push(input);
+  });
+  const maxLength = Object.keys(inputsByLength).reduce((max, el) => (max > el ? max : el));
+  return inputsByLength[maxLength];
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
