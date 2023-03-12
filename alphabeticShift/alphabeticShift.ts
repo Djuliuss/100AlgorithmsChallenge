8f5678e5274c667fe9be549136ff45973831cfd6
Julio Delgado <juliods@gmail.com>
2023-03-12 15:09:26 +0000

    alphabetic shift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,10 @@
 export function alphabeticShift(inputString: string): string {
-
+  return inputString.split("").map(nextCharacter).join("");
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift("crazy"));
+
+function nextCharacter(char: string) {
+  const nextCharacter = char.charCodeAt(0) === 122 ? 97 : char.charCodeAt(0) + 1;
+  return String.fromCharCode(nextCharacter);
+}
