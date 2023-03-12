046d6953ef5544506122141670be7016bc8f9daf
Julio Delgado <juliods@gmail.com>
2023-03-12 15:25:28 +0000

    alphabet sequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,17 @@
 export function alphabetSubsequence(s: string): boolean {
-
+  let response = true;
+  for (let index = 1; index < s.length; index++) {
+    const [current, previous] = [s.charAt(index), s.charAt(index - 1)];
+    if (current.charCodeAt(0) <= previous.charCodeAt(0)) {
+      response = false;
+      break;
+    }
+  }
+  return response;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence("zab"));
+console.log(alphabetSubsequence("effg"));
+console.log(alphabetSubsequence("cdce"));
+console.log(alphabetSubsequence("ace"));
+console.log(alphabetSubsequence("bxz"));
