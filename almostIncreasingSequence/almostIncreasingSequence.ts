cb58f05f7bff3cd821f9eeb71a28b7296e97a7f8
Julio Delgado <juliods@gmail.com>
2023-03-12 14:36:35 +0000

    almost increaseing

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,19 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
-
+  let foundElementThatNeedsRemoving = false;
+  let response = true;
+  for (let index = 1; index < sequence.length; index++) {
+    const [current, previous] = [sequence[index], sequence[index - 1]];
+    if (current <= previous) {
+      if (foundElementThatNeedsRemoving) {
+        response = false;
+        break;
+      } else {
+        foundElementThatNeedsRemoving = true;
+      }
+    }
+  }
+  return response;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+// console.log(almostIncreasingSequence([1, 3, 2, 1]))
+// console.log(almostIncreasingSequence([1, 3, 2]))
