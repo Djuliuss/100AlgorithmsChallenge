e55156fd1a7099ead42fa7b0e8d33367b0817774
Julio Delgado <juliods@gmail.com>
2023-03-12 15:39:05 +0000

    are equally strong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,9 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+  const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+  const friendsStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+  const yourTotalStrength = yourLeft + yourRight;
+  const friendsTotalStrength = friendsLeft + friendsRight;
+  return yourStrongest === friendsStrongest && yourTotalStrength === friendsTotalStrength;
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
