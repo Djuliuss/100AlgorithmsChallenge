3040386d1d72e972dd4464be91e0b9aa63a444b1
Julio Delgado <juliods@gmail.com>
2023-03-12 14:08:36 +0000

    adjacentElements

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,15 @@
-export function adjacentElementsProduct(inputArray: number[]): number {
+// export function adjacentElementsProduct(inputArray: number[]): number {
+//   return inputArray.reduce((max, _, index) => {
+//     if (index !== inputArray.length) {
+//       const product = inputArray[index] * inputArray[index + 1];
+//       max = product > max ? product : max;
+//     }
+//     return max;
+//   });
+// }
 
+export function adjacentElementsProduct(inputArray: number[]): number {
+  return inputArray.reduce((max, _, index) => (inputArray[index] * inputArray[index + 1] > max ? inputArray[index] * inputArray[index + 1] : max));
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+// console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
